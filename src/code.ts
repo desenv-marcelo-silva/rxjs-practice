6cf1e12c2b388fe8c08ffeab508e521d98d2e8d2
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2022-06-04 11:17:04 -0300

    subjects

--- src/code.ts
+++ src/code.ts
@@ -1,10 +1,24 @@
-import { fromEvent } from 'rxjs';
+import { Subject } from 'rxjs';
 
-var observable = fromEvent(document, 'mousemove');
+var subject = new Subject();
 
-setTimeout(() => {
-  var subscription = observable.subscribe((x: any) => addItem(x));
-}, 2000);
+subject.subscribe({
+  next: (data: any) => addItem('Observe 1: ' + data),
+  error: (err) => addItem(err),
+  complete: () => addItem('Observer 1 completed'),
+});
+
+subject.next('The first thing has been sent');
+
+var observer2 = subject.subscribe({
+  next: (data: any) => addItem('Observer 2: ' + data),
+});
+subject.next('The second thing has been sent');
+subject.next('A third thing has been sent');
+
+observer2.unsubscribe();
+
+subject.next('A final thing has been sent');
 
 function addItem(val: any) {
   var node = document.createElement('li');
